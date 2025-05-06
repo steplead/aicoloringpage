777959704dc6e4f846e417b893f1c9261895a135
steplead <aquamo@gmail.com>
2025-05-06 09:51:13 +0800

    更新Supabase配置和环境变量设置

--- next.config.js
+++ next.config.js
@@ -5,6 +5,21 @@ const nextConfig = {
   images: {
     domains: ['ai-coloringpage.com', 'www.ai-coloringpage.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'ai-coloringpage.com',
+          },
+        ],
+        destination: 'https://www.ai-coloringpage.com',
+        permanent: true,
+      },
+    ]
+  },
   async headers() {
     return [
       {
