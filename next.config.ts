fdd9e2ab2b750fbb7cb202bca2881a6079c33baa
steplead <aquamo@gmail.com>
2025-12-10 20:40:06 +0800

    fix: remove strict type from next config to allow build ignores

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
