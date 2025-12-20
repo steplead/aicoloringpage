c727c5b4948179603a930b719747f06ac19fc699
steplead <aquamo@gmail.com>
2025-12-20 20:46:03 +0800

    fix: restore missing imports in routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,3 +1,6 @@
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+
 export const routing = defineRouting({
     // A list of all locales that are supported
     locales: ['en', 'es', 'pt', 'fr'],
